9f675788826a867f64714b5d97c1af65d9a4b919
Shu Chao /Daniel Kuang <danielkuang7@gmail.com>
2018-12-03 15:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var allMusicians = [];
+  for (var i=0; i < musicians.length; i++){
+  allMusicians.push(musicians[i] + " plays " + instruments[i]);
+}
+  return allMusicians;
+}
+
+function johnLennonFacts(arr){
+  var result = [];
+  var i = 0;
+  while(i < arr.length){
+  result.push(arr[i] + '!!!')
+  i++;
+}
+  return result;
+}
+
+function iLoveTheBeatles(number){
+
+  let newArray = [];
+  do {
+    newArray.push(`I love the Beatles!`);
+    number++;
+     } while (number < 15);
+  return newArray;
+}
