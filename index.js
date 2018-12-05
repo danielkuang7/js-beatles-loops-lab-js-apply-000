189f86d1e6437b89d4b0a90342464ff23f580aec
Shu Chao /Daniel Kuang <danielkuang7@gmail.com>
2018-12-05 00:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = [];
+  for(var i =0; i < musicians.length; i++) {
+    newArr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(arr) {
+var result= [];
+var i = 0;
+while(i < arr.length) {
+result.push(arr[i] + '!!!')
+i++;
+ }
+return result;
+}
+
+function iLoveTheBeatles(number) {
+
+let newArray = [];
+do {
+newArray.push(`I love the Beatles!`); //this will happen one time, whether or not number meets condition below
+number++; //increment number of times string above is pushed into newArray
+} while (number < 15); //(that number subracted from 15 will be the output number of times the string is logged);
+
+return newArray; // this needs to be outside the loop, otherwise only first item will be returned
+}
